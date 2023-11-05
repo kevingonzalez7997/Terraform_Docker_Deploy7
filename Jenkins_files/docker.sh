41407a0c094a2e50e735179a4429e91b400055b6
Kevin Gonzalez <59447523+kevingonzalez7997@users.noreply.github.com>
2023-11-04 23:27:53 -0400

    Create docker.sh

---
+++ Jenkins_files/docker.sh
@@ -0,0 +1,40 @@
+sudo apt-get update
+
+sudo apt install -y deafult-jre
+
+sudo apt update
+
+
+sudo apt-get install -y ca-certificates curl gnupg
+
+sudo install -m 0755 -d /etc/apt/keyrings
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+
+
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo apt update
+
+sudo apt install -y software-properties-common
+
+sudo add-apt-repository -y ppa:deadsnakes/ppa
+
+sudo apt install -y python3.7
+
+sudo apt install -y python3.7-venv
+
+sudo apt install -y build-essential
+
+sudo apt install -y libmysqlclient-dev
+
+sudo apt install -y python3.7-dev
